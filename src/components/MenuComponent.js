ba5d5bd17678b3171c1038fb8a8f6635d3d4fc89
rohniRam <roshni.ram001@gmail.com>
2018-09-19 23:06:41 +0530

    Presentation and container components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import DishDetail from './DishdetailComponent';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
