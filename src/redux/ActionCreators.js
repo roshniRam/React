c4609bfc3b0a35518bf1e293edf2cf9aeda4b0b1
roshniRam <roshni.ram001@gmail.com>
2018-10-09 15:45:46 +0530

    Assignment 4- Task 1

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -114,7 +114,7 @@ export const fetchPromos = () => (dispatch) => {
 
     dispatch(promosLoading(true));
 
-    return fetch(baseUrl + 'dishes')
+    return fetch(baseUrl + 'promos')
         .then(response => {
                 if (response.ok) {
                     return response;
@@ -129,7 +129,7 @@ export const fetchPromos = () => (dispatch) => {
                 throw errmess;
             })
         .then(response => response.json()
-            .then(dishes => dispatch(addPromos(dishes))))
+        .then(promos => dispatch(addPromos(promos))))
         .catch(error => dispatch(promosFailed(error.message)));;
 }
 
@@ -145,4 +145,36 @@ export const promosFailed = (errmess) => ({
 export const addPromos = (promos) => ({
     type: ActionTypes.ADD_PROMOS,
     payload: promos
+});
+
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading(true));
+     return fetch(baseUrl + 'leaders')
+        .then(response => {
+            if(response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, 
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .catch(error => dispatch(leadersFailed(error.message)));
+}
+ export const leadersLoading = () => ({
+    type: ActionTypes.LEADERS_LOADING
+});
+ export const leadersFailed = (errmess) => ({
+    type: ActionTypes.LEADERS_FAILED,
+    payload: errmess
+});
+ export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders
 });
